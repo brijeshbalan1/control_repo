5e9b1a6fbda47525af234a504b53a428b74eada2
brijeshbalan1 <62543622+brijeshbalan1@users.noreply.github.com>
2020-11-29 08:30:17 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,6 @@
-node l01d18704150001.wlrt1haumscuvoo0leiupt1dbh.bx.internal.cloudapp.net {
-  file {'/home/admindhl/BRIJESHREADME':
+node default {
+  file {'/root/README':
     ensure => file,
+    content => 'This is readme'
   }
 }
